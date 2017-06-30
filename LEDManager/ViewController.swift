f8eb37ba1b56cb4ed08b01fdd5bf0ed4eeca4e45
duruldalkanat <durul@bluewhaleapps.com>
2017-06-30 17:04:04 -0400

    LED Manager
    First commit.

--- LEDManager/ViewController.swift
+++ LEDManager/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ledSwitch: UISwitch!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    var ref: DatabaseReference?
+    var switchRef: DatabaseReference?
+    
+    //MARK: - Super Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        FirebaseApp.configure()
+        ref = Database.database().reference()
+        switchRef = ref?.child("ledSwitch/state")
+        
+        Auth.auth().signIn(withEmail: "adurul17@hotmail.com", password: "1234567") { (user, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            } else {
+                print("Signed in Succesfully \(String(describing: user?.email))")
+                self.observerFirebaseSwitch()
+            }
+        }
+    }
+    
+    @IBAction func switchValueChanged(_ sender: UISwitch) {
+        switch sender.isOn {
+        case true:
+            switchRef?.setValue(1)
+            updateStatus(value: true)
+        default:
+            switchRef?.setValue(0)
+            updateStatus(value: false)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +55,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func observerFirebaseSwitch() {
+        switchRef?.observe(.value, with: { (data) in
+            if let val = data.value as? Int {
+                self.ledSwitch.setOn(Bool(val as NSNumber), animated: true)
+                self.updateStatus(value: self.ledSwitch.isOn)
+            }
+        })
+    }
 
+    func updateStatus(value:Bool) {
+        value ? (statusLabel.text = "Switch Status OFF") : (statusLabel.text = "Switch Status OFF")
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'LEDManager' do
+  pod 'Firebase'
+  pod 'Firebase/Database'
+  pod 'Firebase/Auth'
+end
